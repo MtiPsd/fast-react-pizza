9d8c362c6fcd997a3ea73ad19aa87a2056db7b56
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-10-10 17:28:27 +0330

    DONE :: loading menu from api

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import {
   RouterProvider,
   createBrowserRouter,
 } from "react-router-dom";
-
 import Home from "./ui/Home";
+import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Order from "./features/order/Order";
 import Cart from "./features/cart/Cart";
@@ -14,9 +14,15 @@ import AppLayout from "./ui/AppLayout";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu />, loader: menuLoader },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
       { path: "/order/:orderId", element: <Order /> },
