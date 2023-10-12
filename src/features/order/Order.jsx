8aa6b618b37c05c84e9cd259cf7c5146073ceab8
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-10-12 18:53:10 +0330

    Learned :: about spacing in tailwind

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -37,9 +37,7 @@ function Order() {
       <div>
         <p>
           {deliveryIn >= 0
-            ? `Only ${calcMinutesLeft(
-                estimatedDelivery,
-              )} minutes left 😃`
+            ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
             : "Order should have arrived"}
         </p>
         <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
@@ -47,13 +45,8 @@ function Order() {
 
       <div>
         <p>Price pizza: {formatCurrency(orderPrice)}</p>
-        {priority && (
-          <p>Price priority: {formatCurrency(priorityPrice)}</p>
-        )}
-        <p>
-          To pay on delivery:{" "}
-          {formatCurrency(orderPrice + priorityPrice)}
-        </p>
+        {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
+        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
       </div>
     </div>
   );
