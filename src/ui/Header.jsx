8a412ff7795bef8df2ae218f2e3504e0201a3995
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-10-14 17:11:45 +0330

    Finished :: styling with tailwind

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,21 +1,16 @@
 import { Link } from "react-router-dom";
-import SearchOrder from "./../features/order/SearchOrder";
-import Username from "./../features/user/Username";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header
-      className="sm:px-6z flex items-center justify-between border-b
-     border-stone-500 bg-yellow-400 px-4 py-4 uppercase"
-    >
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
 
       <SearchOrder />
-
       <Username />
-      <p>MTI</p>
     </header>
   );
 }
