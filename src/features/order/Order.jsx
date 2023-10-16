ca0341365afcc821c66db6030b4a559a38d1a714
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-10-16 23:27:27 +0330

    Implement :: useFetcher to fetch the remote data without loader (if you are using react-router-dom)

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,8 +1,5 @@
 // Test ID: IIDSAT
-
-import OrderItem from "./OrderItem";
-
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
@@ -10,8 +7,19 @@ import {
   formatDate,
 } from "../../utils/helpers";
 
+import OrderItem from "./OrderItem";
+import { useEffect } from "react";
+
 function Order() {
   const order = useLoaderData();
+  // get remote state without needing a loader
+  const fetcher = useFetcher();
+
+  useEffect(() => {
+    if (!fetcher.data && fetcher.state === "idle") {
+      fetcher.load("/menu");
+    }
+  }, [fetcher]);
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
@@ -56,7 +64,15 @@ function Order() {
 
       <ul className="dive-stone-200 divide-y border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === "loading"}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
 
